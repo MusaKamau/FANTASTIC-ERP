cb7e4ffc30bac8da63efebf3b7c1b9b5e202ff55
GithungoIan <githungoian53@gmail.com>
2020-08-25 13:53:46 +0300

    import dev data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Work = require('../../models/workModel');
+const User = require('../../models/userModel');
+
+dotenv.config({ path: '../../config.env' });
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB, connection successfully'));
+
+// Read the file contents
+const work = JSON.parse(fs.readFileSync(`${__dirname}/work.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+
+// Import data into the database
+const importData = async () => {
+  try {
+    await Work.create(work);
+    console.log('Data successfully loaded');
+    await User.create(users, { validateBeforeSave: false });
+    // console.log('Data successfully imported');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//delete data from db
+const deleteData = async () => {
+  try {
+    await Work.deleteMany();
+    await User.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}

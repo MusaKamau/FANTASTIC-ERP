db4fa280b31a8b603ecd14a21d8523917f243288
GithungoIan <githungoian53@gmail.com>
2020-08-25 13:40:58 +0300

    Database connection

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB, connection successfully'));
 
 const port = process.env.PORT;
 const server = app.listen(port, () => {

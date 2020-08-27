0f4dac8069409fbf19a631cb7685e7bfb9d2a616
GithungoIan <githungoian53@gmail.com>
2020-08-27 11:48:53 +0300

    refactor api features

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,11 +1,13 @@
 const Job = require('../models/jobModel');
 
-// Get all Job postings available in the database
-exports.getAllJobs = async (req, res) => {
-  try {
-    // BUILD QUERY
-    // 1) Filtering
-    const queryObj = { ...req.query };
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach((el) => delete queryObj[el]);
 
@@ -13,43 +15,59 @@ exports.getAllJobs = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    let query = Job.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
 
+    return this;
+  }
+
+  sort() {
     // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
     } else {
-      query = query.sort('-datePosted');
+      this.query = this.query.sort('-datePosted');
     }
 
-    // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
     } else {
-      query = query.select('-__v');
+      this.query = this.query.select('-__v');
     }
 
-    // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
 
-    query = query.skip(skip).limit(limit);
+    this.query = this.query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      const numJob = await Job.countDocuments();
-      if (skip >= numJob) throw new Error('This page does not exists');
-    }
-
+    return this;
+  }
+}
+// Get all Job postings available in the database
+exports.getAllJobs = async (req, res) => {
+  try {
     // EXECUTE QUERY
-    const jobs = await query;
+    const features = new APIFeatures(Job.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const jobs = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
       status: 'Success',
-      results: Jobs.length,
+      results: jobs.length,
       data: {
         jobs,
       },

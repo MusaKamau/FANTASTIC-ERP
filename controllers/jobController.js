14721af549647b78487f589605a45d621523b029
GithungoIan <githungoian53@gmail.com>
2020-09-03 16:46:24 +0300

    refactor to handle catchAsync

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -3,50 +3,36 @@ const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 
 // Get all Job postings available in the database
-exports.getAllJobs = async (req, res) => {
-  try {
-    // EXECUTE QUERY
-    const features = new APIFeatures(Job.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const jobs = await features.query;
+exports.getAllJobs = catchAsync(async (req, res) => {
+  // EXECUTE QUERY
+  const features = new APIFeatures(Job.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const jobs = await features.query;
 
-    // SEND RESPONSE
-    res.status(200).json({
-      status: 'Success',
-      results: jobs.length,
-      data: {
-        jobs,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-};
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'Success',
+    results: jobs.length,
+    data: {
+      jobs,
+    },
+  });
+});
 
 // Get a single job bsed on it's ID from the database
-exports.getJob = async (req, res) => {
-  try {
-    const singleJob = await Job.findById(req.params.id);
+exports.getJob = catchAsync(async (req, res) => {
+  const singleJob = await Job.findById(req.params.id);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        singleJob,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      singleJob,
+    },
+  });
+});
 
 //Create a new job posting in the database
 exports.createJob = catchAsync(async (req, res, next) => {
@@ -61,40 +47,26 @@ exports.createJob = catchAsync(async (req, res, next) => {
 });
 
 // Update a single Job based on its ID
-exports.updateJob = async (req, res) => {
-  try {
-    const singleJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.updateJob = catchAsync(async (req, res) => {
+  const singleJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        singleJob,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      singleJob,
+    },
+  });
+});
 
 // Delete a job based on its ID from the database
-exports.deleteJob = async (req, res) => {
-  try {
-    await Job.findByIdAndDelete(req.params.id);
+exports.deleteJob = catchAsync(async (req, res) => {
+  await Job.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err.message,
-    });
-  }
-};
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});

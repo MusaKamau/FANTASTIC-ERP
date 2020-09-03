0acad8ad4947e91cce175903bd9e6859228fd30c
GithungoIan <githungoian53@gmail.com>
2020-09-03 15:52:07 +0300

    implement error handling

--- app.js
+++ app.js
@@ -18,10 +18,16 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/jobs', jobRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+app.all('*', (req, res, next) => {});
+
+// Error handling
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'err';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
   next();
 });

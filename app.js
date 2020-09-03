ad86d385702a5b7de0d52a51ce285f21fa9ec1b2
GithungoIan <githungoian53@gmail.com>
2020-09-03 14:36:10 +0300

    handle all unhandlled routes

--- app.js
+++ app.js
@@ -12,13 +12,18 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋 ');
-  next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
 app.use('/api/v1/jobs', jobRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+  next();
+});
 
 module.exports = app;

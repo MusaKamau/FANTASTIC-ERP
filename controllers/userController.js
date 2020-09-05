f732e5c077bc33b7b8d964fcbb096e77b90975bc
GithungoIan <githungoian53@gmail.com>
2020-09-05 15:45:05 +0300

    refactor signup to authcontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,3 @@
-const User = require('../models/userModel');
-const catchAsync = require('../utils/catchAsync');
-
-exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      user: newUser,
-    },
-  });
-});
-
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: 'error',

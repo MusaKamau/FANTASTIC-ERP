e83fa40c30fc14f10eb3b6b00a2411e3eca654d2
GithungoIan <githungoian53@gmail.com>
2020-09-07 17:06:53 +0300

    validate user password

--- models/userModel.js
+++ models/userModel.js
@@ -69,5 +69,12 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+// instance method
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 const User = mongoose.model('User', userSchema);
 module.exports = User;

015b0ecc17e9980eaadd0e4897151b1709dabb80
GithungoIan <githungoian53@gmail.com>
2020-09-07 15:57:45 +0300

    create signup jwtwebtoken

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -9,8 +10,16 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
+  // Create token here
+  // 1) payload
+  // 2) Secret
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       newUser,
     },

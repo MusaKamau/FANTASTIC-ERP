de511aacad9191aece3a198342774b7d1ed8d07a
GithungoIan <githungoian53@gmail.com>
2020-09-07 17:07:40 +0300

    authenticate user

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,11 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -14,10 +19,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   // Create token here
   // 1) payload
   // 2) Secret
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-
+  const token = signToken(newUser._id);
   res.status(201).json({
     status: 'success',
     token,
@@ -36,10 +38,13 @@ exports.login = catchAsync(async (req, res, next) => {
   }
   // 2) Check if user exist and password is correct
   const user = await User.findOne({ email }).select('+password');
-  console.log(user);
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
 
   // 3) if ok send token
-  const token = 'this is the token';
+  const token = signToken(user._id);
   res.status(200).json({
     status: 'success',
     token,

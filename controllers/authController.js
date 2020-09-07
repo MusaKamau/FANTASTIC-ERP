990f4fca9d08cf146c35df49ca5538dc65378d1b
GithungoIan <githungoian53@gmail.com>
2020-09-07 18:26:07 +0300

    check whether user has token

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,3 +50,27 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting the token and check if it exist
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError('You are not loged in please login to gain access', 401)
+    );
+  }
+  // 2) Verification token
+
+  // 3) Check if user still exists
+
+  // 4) Check if user changed password after the token ws issues
+  next();
+});

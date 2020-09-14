b350b509d21e39774839f7c6a0458ea16dd9c651
GithungoIan <githungoian53@gmail.com>
2020-09-14 07:38:57 +0300

    refactor send jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,18 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+/*
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000

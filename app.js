e14c9eeee2fac6bf0000aab09fc765dfd23e6b45
GithungoIan <githungoian53@gmail.com>
2020-09-14 11:19:04 +0300

    secure HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,25 +11,33 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // GLOBAL Middleware
+//  Set Security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit request from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this Ip, Please try again in an hour!',
 });
-
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
 app.use('/api/v1/jobs', jobRouter);
 app.use('/api/v1/users', userRouter);
 
+// Test middleware
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 400));
 });

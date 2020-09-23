b9e54dec8b7842d86fa3d7bde902b8f348cddb01
GithungoIan <githungoian53@gmail.com>
2020-09-23 18:15:02 +0300

    login and signup routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', viewsController.landing);
 router.get('/overview', viewsController.getOverview)
 router.get('/job', viewsController.jobDetail)
-router.get('/login', viewsController.login)
-router.get('/signup', viewsController.signup)
+router.get('/login', viewsController.getLoginForm)
+router.get('/signup', viewsController.getSignupForm)
 
 module.exports = router

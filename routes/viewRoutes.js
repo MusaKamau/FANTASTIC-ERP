7b07764bc33d87d4b810cbd8703bc862eb612674
GithungoIan <githungoian53@gmail.com>
2020-09-24 08:32:56 +0300

    check if user is logged in

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 const router = express.Router();
 
+router.use(authController.isLoggedIn);
 router.get('/', viewsController.landing);
-router.get('/overview', viewsController.getOverview)
-router.get('/job', viewsController.jobDetail)
+router.get('/overview',authController.protect,  viewsController.getOverview)
+router.get('/job', authController.protect, viewsController.jobDetail)
 router.get('/login', viewsController.getLoginForm)
 router.get('/signup', viewsController.getSignupForm)
 

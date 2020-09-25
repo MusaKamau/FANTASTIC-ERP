4b29d0df2517501e62db9b16d8b07eb72ef9dda8
GithungoIan <githungoian53@gmail.com>
2020-09-25 12:46:07 +0300

    render static pages

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,7 @@
 const Job = require('../models/jobModel');
+const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync( async(req, res, next) => {
   // Get Job data from collection
@@ -16,11 +18,19 @@ exports.landing =  (req, res) => {
   res.status(200).render('landingPage');
 }
 
-exports.jobDetail = (req, res) => {
+exports.jobDetail = catchAsync(async(req, res, next) => {
+  // 1) Get the data for the requested job
+  const job = await Job.findOne({slug: req.params.slug});
+  if(!job) {
+    return next(new AppError('There is no job with that name.', 404));
+  }
+  // 2) Build template
+  // 3) Render the template using data from 1
   res.status(200).render('job', {
-    title: 'Job'
+    title: `${job.name}`,
+    job
   })
-}
+});
 
 exports.getLoginForm = (req, res) => {
   res.status(200).render('login', {
@@ -29,5 +39,11 @@ exports.getLoginForm = (req, res) => {
 }
 
 exports.getSignupForm = (req, res) => {
-  res.status(200).render('signup');
+  res.status(200).render('signup', {
+    title: 'Signup'
+  });
+}
+
+exports.getForgotPasswordForm = (req, res) => {
+  res.status(200).render('forgotPassword');
 }

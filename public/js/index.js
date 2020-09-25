900df76fbc0b26b324313192fa2b7e0b5f97d0af
GithungoIan <githungoian53@gmail.com>
2020-09-25 12:48:11 +0300

    serve static js file

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,13 @@
 
 import '@babel/polyfill';
 import {login} from './login';
+import {signup} from './signup';
+import {navSlide} from './script'
+
 
 // Dom Elements
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form__login');
+const signupForm = document.querySelector('.form__signup');
 
 
 // Delegation
@@ -15,3 +19,17 @@ if (loginForm){
     login(email, password)
   })
 }
+
+if(signupForm){
+  signupForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+    signup(name, email, password, passwordConfirm);
+  })
+}
+
+
+navSlide()

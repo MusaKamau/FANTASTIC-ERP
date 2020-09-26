f0bef445d6b0adae7d3d3072a43815a0855b57a6
GithungoIan <githungoian53@gmail.com>
2020-09-26 12:56:43 +0300

    implement user account routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -10,5 +10,6 @@ router.get('/job', authController.protect, viewsController.jobDetail)
 router.get('/login', viewsController.getLoginForm)
 router.get('/signup', viewsController.getSignupForm)
 router.get('/forgotPassword', viewsController.getForgotPasswordForm)
+router.get('/account', viewsController.getAccountPage);
 
 module.exports = router

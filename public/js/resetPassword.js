04ed1900e8da595fe95c5e8059a2831f2e69f067
GithungoIan <githungoian53@gmail.com>
2020-09-28 11:24:45 +0300

    Reset user password

---
+++ public/js/resetPassword.js
@@ -0,0 +1,25 @@
+import '@babel/polyfill'
+import axios from 'axios';
+import {showAlert} from './alerts';
+
+
+export const resetPassword = async(password, passwordConfirm) => {
+  try{
+    const res = await axios({
+      method: 'PATCH',
+      url : 'http://127.0.0.1:3000/api/v1/users/resetPassword/:token',
+      data: {
+        password,
+        passwordConfirm,
+      }
+    });
+    if(res.data.status === 'success') {
+      showAlert('success', 'password reset successful!');
+      window.setTimeout(() => {
+        location.assign('/login');
+      }, 1000);
+    }
+  }catch (err) {
+    showAlert('Error', err.response.data.message);
+  }
+}

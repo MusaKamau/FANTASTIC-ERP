f811c8fc69b4a7e83eef968a014d290bf28b48b1
GithungoIan <githungoian53@gmail.com>
2020-10-08 07:57:14 +0300

    send welcome email

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -47,14 +47,16 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
-
+  const url = `${req.protocol}://${req.get('host')}/account`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
   // Send thr tokne here
   res.status(201).json({
     status: 'success',
     data: {
-      newUser
-    }
-  })
+      newUser,
+    },
+  });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -78,13 +80,13 @@ exports.login = catchAsync(async (req, res, next) => {
 exports.logout = (req, res) => {
   res.cookie('jwt', 'logged out', {
     expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true
+    httpOnly: true,
   });
 
   res.status(200).json({
-    status: 'success'
-  })
-}
+    status: 'success',
+  });
+};
 
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting the token and check if it exist
@@ -94,7 +96,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
-  } else if(req.cookies.jwt) {
+  } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
   }
   // console.log(token);
@@ -130,13 +132,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   return next();
 });
 
-
 // Only for rendered pages and there will be no errors
 exports.isLoggedIn = async (req, res, next) => {
-  if(req.cookies.jwt) {
-    try{
+  if (req.cookies.jwt) {
+    try {
       // 1) Verification token
-      const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
 
       // 2) Check if user still exists
       const freshUser = await User.findById(decoded.id);
@@ -152,8 +156,8 @@ exports.isLoggedIn = async (req, res, next) => {
       //There is a logged in user
       res.locals.user = freshUser;
       return next();
-    }catch (err) {
-      return next()
+    } catch (err) {
+      return next();
     }
   }
   next();
@@ -188,11 +192,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   const message = `Forgot your password? submit a patch request with your new password and passwordConfirm to : ${resetURL}.\n If you didn't forget your password, please ignore this email`;
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token valid for (10 min)',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token valid for (10 min)',
+    //   message,
+    // });
 
     res.status(200).json({
       status: 'success',
@@ -257,4 +261,4 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
   // 4) Log user in, send jwt
   createSendToken(user, 200, res);
-})
+});
